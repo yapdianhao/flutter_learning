393ebd48f8be5fff79da07feadc48e7c0cfea92d
yapdianhao <dianhao.yap@2359media.com>
2021-01-04 23:52:51 +0800

    4/1/2021 194

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:univus_app/models/product.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = "/product-detail";
@@ -7,9 +9,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-    
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((prod) => prod.id == productId);
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(title: Text(loadedProduct.title)),
     );
   }
 }
