88cd7764756d17414c49b1ee54db701c34ec3bc8
yapdianhao <dianhao.yap@2359media.com>
2021-01-13 03:10:03 +0800

    13/1/2021 203

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart';
+import '../widgets/cart_item.dart';
+import '../providers/cart.dart' show Cart;
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -45,8 +46,24 @@ class CartScreen extends StatelessWidget {
                           textColor: Theme.of(context).primaryColor,
                         )
                       ],
-                    )))
-          ],
-        ));
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cart.itemCount,
+                    itemBuilder: (ctx, i) => 
+                      CartItem(
+                        cart.items.values.toList()[i].id, 
+                        cart.items.values.toList()[i].price, 
+                        cart.items.values.toList()[i].quantity, 
+                        cart.items.values.toList()[i].title
+                      )
+                  )
+                )
+              ],
+            ),
+          );
   }
 }
