f4ee55a8086fc5d5b581510543a17d0d0f5104d1
yapdianhao <dianhao.yap@2359media.com>
2021-01-15 01:43:40 +0800

    15/1/2021 206

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:univus_app/providers/orders.dart';
 
 import './screens/products_overview_screen.dart';
 import './screens/cart_screen.dart';
@@ -22,6 +23,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
         ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
+        )
       ],
       child: MaterialApp(
         title: 'MyShop',
