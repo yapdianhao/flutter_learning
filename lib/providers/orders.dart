0c8e1113ca880345a4ee927bccf5503651138dce
yapdianhao <dianhao.yap@2359media.com>
2021-01-16 00:00:47 +0800

    16/1/2021 207

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -13,7 +13,7 @@ class OrderItem {
     @required this.amount,
     @required this.products,
     @required this.dateTime,
-  })
+  });
 }
 
 class Orders with ChangeNotifier {
@@ -24,11 +24,13 @@ class Orders with ChangeNotifier {
   }
 
   void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(0, OrderItem(
-      id: DateTime.now().toString(), 
-      amount: total, 
-      dateTime: DateTime.now(), 
-      products: cartProducts));
+    _orders.insert(
+        0,
+        OrderItem(
+            id: DateTime.now().toString(),
+            amount: total,
+            dateTime: DateTime.now(),
+            products: cartProducts));
     notifyListeners();
   }
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../widgets/cart_item.dart';
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -12,59 +13,60 @@ class CartScreen extends StatelessWidget {
     final cart = Provider.of<Cart>(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Your Cart'),
-        ),
-        body: Column(
-          children: [
-            Card(
-                margin: EdgeInsets.all(15),
-                child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text('total',
-                            style: TextStyle(
-                              fontSize: 20,
-                            )),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Chip(
-                          label: Text(
-                            '\$${cart.totalAmount}',
-                            style: TextStyle(
-                              color: Theme.of(context).primaryTextTheme.bodyText1.color,
-                            )
-                          ),
-                          backgroundColor: Theme.of(context).primaryColor,
-                        ),
-                        FlatButton(
-                          onPressed: null, 
-                          child: Text('Order now'),
-                          textColor: Theme.of(context).primaryColor,
-                        )
-                      ],
-                    ),
+      appBar: AppBar(
+        title: Text('Your Cart'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('total',
+                      style: TextStyle(
+                        fontSize: 20,
+                      )),
+                  SizedBox(
+                    width: 10,
                   ),
-                ),
-                SizedBox(height: 10),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: cart.itemCount,
-                    itemBuilder: (ctx, i) => 
-                      CartItem(
-                        cart.items.values.toList()[i].id, 
-                        cart.items.keys.toList()[i],
-                        cart.items.values.toList()[i].price, 
-                        cart.items.values.toList()[i].quantity, 
-                        cart.items.values.toList()[i].title
-                      )
+                  Chip(
+                    label: Text('\$${cart.totalAmount}',
+                        style: TextStyle(
+                          color: Theme.of(context)
+                              .primaryTextTheme
+                              .bodyText1
+                              .color,
+                        )),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                          cart.items.values.toList(), cart.totalAmount);
+                      cart.clear();
+                    },
+                    child: Text('Order now'),
+                    textColor: Theme.of(context).primaryColor,
                   )
-                )
-              ],
+                ],
+              ),
             ),
-          );
+          ),
+          SizedBox(height: 10),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: cart.itemCount,
+                  itemBuilder: (ctx, i) => CartItem(
+                      cart.items.values.toList()[i].id,
+                      cart.items.keys.toList()[i],
+                      cart.items.values.toList()[i].price,
+                      cart.items.values.toList()[i].quantity,
+                      cart.items.values.toList()[i].title)))
+        ],
+      ),
+    );
   }
 }
