0aadcf3b510121a18566bd1dbdb518a501b5e667
yapdianhao <dianhao.yap@2359media.com>
2021-01-23 11:16:10 +0800

    23/1/2021 215

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -17,6 +17,28 @@ class CartItem extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Confirm delete?'),
+                  content: Text('Do you want to remove item from the cart?'),
+                  actions: [
+                    FlatButton(
+                      child: Text('No'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(false);
+                      },
+                    ),
+                    FlatButton(
+                      child: Text('Yes'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(true);
+                      },
+                    )
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
